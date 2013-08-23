254d4f9b3036883ac7d2e2fead6ec68058ffd996
mariusneo <mariusneo@gmail.com>
2013-08-23 14:18:45 +0200

    On screen orientation change, the viewport of the chartview is not resetted.

--- vitaminclicker/src/com/example/vitaminclicker/StatisticsActivity.java
+++ vitaminclicker/src/com/example/vitaminclicker/StatisticsActivity.java
@@ -32,188 +32,192 @@ public class StatisticsActivity extends Activity {
 	private static final String MONTH_FORMAT = "MMMMM";
 
 	private static final String DAY_FORMAT = "d";
-	
+
 	private static final String DATASET_KEY = "dataset";
 	private static final String RENDERER_KEY = "renderer";
 	private static final String SERIES_KEY = "series";
 
 	private static final int X_AXIS_DAYS_COUNT = 10;
 	private static final int Y_AXIS_VITAMIN_COUNT = 8;
-	
-	private static final int TWELVE_HOURS = 12* 60*60*1000;
+
+	private static final int TWELVE_HOURS = 12 * 60 * 60 * 1000;
 
 	private XYMultipleSeriesDataset mDataset;
 	private XYMultipleSeriesRenderer mRenderer;
 	private TimeSeries mSeries;
 	private GraphicalView mGraphicalView;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Calendar aCalendar = Calendar.getInstance();
-		resetTime(aCalendar);
-		aCalendar.set(Calendar.DATE,
-				aCalendar.getActualMaximum(Calendar.DAY_OF_MONTH));
-		Date lastDateOfCurrentMonth = aCalendar.getTime();
-		int daysInCurrentMonth = aCalendar.get(Calendar.DAY_OF_MONTH);
-		
-		aCalendar.set(Calendar.DAY_OF_MONTH, 1) ;
-		aCalendar.add(Calendar.MONTH, -1);    
-		Date firstDateOfPreviousMonth = aCalendar.getTime();
+		if (savedInstanceState != null) {
+			// restore the current data, for instance when changing the screen
+			// orientation
+			mDataset = (XYMultipleSeriesDataset) savedInstanceState
+					.getSerializable(DATASET_KEY);
+			mRenderer = (XYMultipleSeriesRenderer) savedInstanceState
+					.getSerializable(RENDERER_KEY);
+			mSeries = (TimeSeries) savedInstanceState
+					.getSerializable(SERIES_KEY);
+		}
 
-		Date date = null;
-		Date dayOfMonth = firstDateOfPreviousMonth;
-		int maxVitaminCount = 0;
+		if (mDataset == null || mRenderer == null || mSeries == null) {
 
-		
-		String title = DateFormat.format(MONTH_FORMAT, new Date())
-				.toString();		
-		mSeries = new TimeSeries(title);
-		
-		
-		Calendar now = Calendar.getInstance();
-		VitaminDatabase db = new VitaminDatabase(this);
-		Cursor vitaminCountsCursor = db.readVitaminCounts(firstDateOfPreviousMonth,
-				now.getTime());
-		resetTime(now);
-		
-		if (vitaminCountsCursor.getCount() > 0) {
-			vitaminCountsCursor.moveToFirst();
+			Calendar aCalendar = Calendar.getInstance();
+			resetTime(aCalendar);
+			aCalendar.set(Calendar.DATE,
+					aCalendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+			Date lastDateOfCurrentMonth = aCalendar.getTime();
+			int daysInCurrentMonth = aCalendar.get(Calendar.DAY_OF_MONTH);
 
-			do {
-				try {
-					date = YYYY_MM_DD_FORMAT
-							.parse(vitaminCountsCursor.getString(vitaminCountsCursor
-									.getColumnIndexOrThrow(COLUMN_NAME_ENTRY_DATE)));
-				} catch (ParseException e) {
-					Log.w(TAG, e.getMessage(), e);
-				}
-				int count = vitaminCountsCursor.getInt(vitaminCountsCursor
-						.getColumnIndexOrThrow(COLUMN_NAME_ENTRY_COUNT));
+			aCalendar.set(Calendar.DAY_OF_MONTH, 1);
+			aCalendar.add(Calendar.MONTH, -1);
+			Date firstDateOfPreviousMonth = aCalendar.getTime();
 
-				if (date != null) {
-					Calendar dayCalendar = Calendar.getInstance();
-					dayCalendar.setTime(date);
-					Date currentDayOfMonth = dayCalendar.getTime();
+			Date date = null;
+			Date dayOfMonth = firstDateOfPreviousMonth;
+			int maxVitaminCount = 0;
 
-					if (compareDays(currentDayOfMonth, dayOfMonth) > 0) {
-						// fill the gaps
-						for (; compareDays(dayOfMonth, currentDayOfMonth) < 0;) {
-							mSeries.add(dayOfMonth, 0);
+			String title = DateFormat.format(MONTH_FORMAT, new Date())
+					.toString();
+			mSeries = new TimeSeries(title);
 
-							dayOfMonth = increaseDay(dayOfMonth, 1);
+			Calendar now = Calendar.getInstance();
+			VitaminDatabase db = new VitaminDatabase(this);
+			Cursor vitaminCountsCursor = db.readVitaminCounts(
+					firstDateOfPreviousMonth, now.getTime());
+			resetTime(now);
+
+			if (vitaminCountsCursor.getCount() > 0) {
+				vitaminCountsCursor.moveToFirst();
+
+				do {
+					try {
+						date = YYYY_MM_DD_FORMAT
+								.parse(vitaminCountsCursor.getString(vitaminCountsCursor
+										.getColumnIndexOrThrow(COLUMN_NAME_ENTRY_DATE)));
+					} catch (ParseException e) {
+						Log.w(TAG, e.getMessage(), e);
+					}
+					int count = vitaminCountsCursor.getInt(vitaminCountsCursor
+							.getColumnIndexOrThrow(COLUMN_NAME_ENTRY_COUNT));
+
+					if (date != null) {
+						Calendar dayCalendar = Calendar.getInstance();
+						dayCalendar.setTime(date);
+						Date currentDayOfMonth = dayCalendar.getTime();
+
+						if (compareDays(currentDayOfMonth, dayOfMonth) > 0) {
+							// fill the gaps
+							for (; compareDays(dayOfMonth, currentDayOfMonth) < 0;) {
+								mSeries.add(dayOfMonth, 0);
+
+								dayOfMonth = increaseDay(dayOfMonth, 1);
+							}
 						}
+
+						if (count > maxVitaminCount) {
+							maxVitaminCount = count;
+						}
+
+						mSeries.add(dayOfMonth, count);
+						dayOfMonth = increaseDay(dayOfMonth, 1);
 					}
 
-					if (count > maxVitaminCount) {
-						maxVitaminCount = count;
-					}
+				} while (vitaminCountsCursor.moveToNext());
 
-					mSeries.add(dayOfMonth, count);
-					dayOfMonth = increaseDay(dayOfMonth, 1);
-				}
+			}
 
-			} while (vitaminCountsCursor.moveToNext());
+			vitaminCountsCursor.close();
+			db.close();
+
+			for (; compareDays(dayOfMonth, now.getTime()) <= 0;) {
+				// fill the last gaps
+				mSeries.add(dayOfMonth, 0);
+
+				dayOfMonth = increaseDay(dayOfMonth, 1);
+			}
+
+			mDataset = new XYMultipleSeriesDataset();
+			mDataset.addSeries(mSeries);
+
+			int[] colors = new int[] { Color.BLUE };
+			PointStyle[] styles = new PointStyle[] { PointStyle.CIRCLE };
+			mRenderer = buildRenderer(colors, styles);
+			int length = mRenderer.getSeriesRendererCount();
+			for (int i = 0; i < length; i++) {
+				((XYSeriesRenderer) mRenderer.getSeriesRendererAt(i))
+						.setFillPoints(true);
+			}
+
+			double minX;
+			double maxX;
+			Date actualDayOfMonth = now.getTime();
+			int actualDayOfMonthIndex = now.get(Calendar.DAY_OF_MONTH);
+			if (actualDayOfMonthIndex + (X_AXIS_DAYS_COUNT / 2) > daysInCurrentMonth) {
+				minX = increaseDay(lastDateOfCurrentMonth, -X_AXIS_DAYS_COUNT)
+						.getTime();
+				maxX = lastDateOfCurrentMonth.getTime();
+			} else {
+				minX = increaseDay(actualDayOfMonth, -(X_AXIS_DAYS_COUNT / 2))
+						.getTime();
+				maxX = increaseDay(actualDayOfMonth, (X_AXIS_DAYS_COUNT / 2))
+						.getTime();
+			}
+
+			setChartSettings(mRenderer, title, getString(R.string.day),
+					getString(R.string.vitamin_counts), minX, maxX, 0,
+					Y_AXIS_VITAMIN_COUNT, Color.BLACK,
+					getResources().getColor(R.color.black));
+
+			mRenderer.setPanLimits(new double[] {
+					firstDateOfPreviousMonth.getTime() - TWELVE_HOURS,
+					lastDateOfCurrentMonth.getTime() + TWELVE_HOURS,
+					0,
+					maxVitaminCount > Y_AXIS_VITAMIN_COUNT ? maxVitaminCount
+							: Y_AXIS_VITAMIN_COUNT });
 
 		}
 		
-		vitaminCountsCursor.close();
-		db.close();
 		
-		for (; compareDays(dayOfMonth, now.getTime()) <= 0;) {
-			// fill the last gaps
-			mSeries.add(dayOfMonth, 0);
-
-			dayOfMonth = increaseDay(dayOfMonth, 1);
-		}
-
-		mDataset = new XYMultipleSeriesDataset();
-		mDataset.addSeries(mSeries);
-
-		int[] colors = new int[] { Color.BLUE };
-		PointStyle[] styles = new PointStyle[] { PointStyle.CIRCLE };
-		mRenderer = buildRenderer(colors, styles);
-		int length = mRenderer.getSeriesRendererCount();
-		for (int i = 0; i < length; i++) {
-			((XYSeriesRenderer) mRenderer.getSeriesRendererAt(i))
-					.setFillPoints(true);
-		}
-
-		double minX;
-		double maxX;
-		Date actualDayOfMonth = now.getTime();
-		int actualDayOfMonthIndex = now.get(Calendar.DAY_OF_MONTH);
-		if (actualDayOfMonthIndex + (X_AXIS_DAYS_COUNT / 2) > daysInCurrentMonth) {
-			minX = increaseDay(lastDateOfCurrentMonth, - X_AXIS_DAYS_COUNT).getTime();
-			maxX = lastDateOfCurrentMonth.getTime();
-		} else {
-			minX = increaseDay(actualDayOfMonth,  - (X_AXIS_DAYS_COUNT / 2)).getTime();
-			maxX = increaseDay(actualDayOfMonth,  (X_AXIS_DAYS_COUNT / 2)).getTime();
-		}
-		
-		setChartSettings(mRenderer, title, getString(R.string.day),
-				getString(R.string.vitamin_counts), minX, maxX, 0,
-				Y_AXIS_VITAMIN_COUNT, Color.BLACK,
-				getResources().getColor(R.color.black));
-		
-		mRenderer.setPanLimits(new double[] {
-				firstDateOfPreviousMonth.getTime() - TWELVE_HOURS,
-				lastDateOfCurrentMonth.getTime() + TWELVE_HOURS,
-				0,
-				maxVitaminCount > Y_AXIS_VITAMIN_COUNT ? maxVitaminCount
-						: Y_AXIS_VITAMIN_COUNT });
-
-		TimeChart chart = new TimeChart(mDataset, mRenderer){
-			public void draw(Canvas canvas, int x, int y, int width, int height, Paint paint) {
+		TimeChart chart = new TimeChart(mDataset, mRenderer) {
+			public void draw(Canvas canvas, int x, int y, int width,
+					int height, Paint paint) {
 				Calendar latestDateShown = Calendar.getInstance();
-				latestDateShown.setTimeInMillis((long)mRenderer.getXAxisMax());
-				String currentMonth = DateFormat.format(MONTH_FORMAT, latestDateShown)
-						.toString();
+				latestDateShown.setTimeInMillis((long) mRenderer.getXAxisMax());
+				String currentMonth = DateFormat.format(MONTH_FORMAT,
+						latestDateShown).toString();
 				mRenderer.setChartTitle(currentMonth);
-				super.draw(canvas,  x,  y,  width,  height, paint);				
+				super.draw(canvas, x, y, width, height, paint);
 			}
 		};
-	    chart.setDateFormat(DAY_FORMAT);
-	    mGraphicalView = new GraphicalView(this, chart);
-		
-	    mGraphicalView.setBackgroundDrawable(getResources().getDrawable(
+		chart.setDateFormat(DAY_FORMAT);
+		mGraphicalView = new GraphicalView(this, chart);
+
+		mGraphicalView.setBackgroundDrawable(getResources().getDrawable(
 				R.color.white));
-		
+
 		setContentView(mGraphicalView);
 	}
-	
-	
-	@Override
-	  protected void onSaveInstanceState(Bundle outState) {
-	    super.onSaveInstanceState(outState);
-	    // save the current data, for instance when changing screen orientation
-	    outState.putSerializable(DATASET_KEY, mDataset);
-	    outState.putSerializable(RENDERER_KEY, mRenderer);
-	    outState.putSerializable(SERIES_KEY, mSeries);
-	  }
 
-	  @Override
-	  protected void onRestoreInstanceState(Bundle savedState) {
-	    super.onRestoreInstanceState(savedState);
-	    // restore the current data, for instance when changing the screen
-	    // orientation
-	    mDataset = (XYMultipleSeriesDataset) savedState.getSerializable(DATASET_KEY);
-	    mRenderer = (XYMultipleSeriesRenderer) savedState.getSerializable(RENDERER_KEY);
-	    mSeries = (TimeSeries) savedState.getSerializable(SERIES_KEY);
-	  }
-	
-	
-  
-	private void resetTime(Calendar c){
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		// save the current data, for instance when changing screen orientation
+		outState.putSerializable(DATASET_KEY, mDataset);
+		outState.putSerializable(RENDERER_KEY, mRenderer);
+		outState.putSerializable(SERIES_KEY, mSeries);
+	}
+
+	private void resetTime(Calendar c) {
 		c.set(Calendar.HOUR_OF_DAY, 0);
 		c.set(Calendar.MINUTE, 0);
 		c.set(Calendar.SECOND, 0);
 		c.set(Calendar.MILLISECOND, 0);
 	}
-	
-	
-	private Date increaseDay(Date date, int value){
+
+	private Date increaseDay(Date date, int value) {
 		Calendar c = Calendar.getInstance();
 		c.setTime(date);
 		c.add(Calendar.DAY_OF_MONTH, value);
@@ -239,7 +243,7 @@ public class StatisticsActivity extends Activity {
 			PointStyle[] styles) {
 		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
 		setRenderer(renderer, colors, styles);
-		
+
 		renderer.setXLabels(X_AXIS_DAYS_COUNT);
 		renderer.setYLabels(Y_AXIS_VITAMIN_COUNT);
 		renderer.setXLabelsColor(Color.BLACK);

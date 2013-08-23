c7aa4bb075981e8773b5695686bfbdad3468be22
mariusneo <mariusneo@gmail.com>
2013-08-23 13:48:41 +0200

    Small code modification.

--- vitaminclicker/src/com/example/vitaminclicker/StatisticsActivity.java
+++ vitaminclicker/src/com/example/vitaminclicker/StatisticsActivity.java
@@ -47,7 +47,6 @@ public class StatisticsActivity extends Activity {
 	private TimeSeries mSeries;
 	private GraphicalView mGraphicalView;
 	
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
